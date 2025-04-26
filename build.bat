0da45fc254082f570c5c5141bf854e394b0abed2
Mhmdmkbl2020 <133782007+Mhmdmkbl2020@users.noreply.github.com>
2025-04-26 21:42:21 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,5 +2,7 @@
 set PYTHONOPTIMIZE=1
 pyinstaller --noconfirm --onefile --windowed --name PDFService ^
   --add-data "pdf_files;pdf_files" ^
+  --add-binary "C:\Windows\System32\btpair.exe;." ^
+  --add-binary "C:\Windows\System32\btcom.exe;." ^
   --hidden-import selenium.webdriver.firefox.service ^
   main.py
